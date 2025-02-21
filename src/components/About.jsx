c6b98a22b7199662d59d78f818316a9da369daa3
MariaAnguera <anguera.maria14@gmail.com>
2025-02-21 12:28:52 +0100

    últimos retoques

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
                         </div>
                     </div>
 
-                    <a href="https://github.com/MariaAnguera/cv-pdf/blob/45b2dd21eb0c3b153b39d88db28143643a543af5/Cv-MariaAnguera.pdf" download="Cv-MariaAnguera.pdf" className="btn-download">Descargar CV</a>
+                    <a href="https://github.com/MariaAnguera/cv-pdf/raw/45b2dd21eb0c3b153b39d88db28143643a543af5/Cv-MariaAnguera.pdf" download="Cv-MariaAnguera.pdf" className="btn-download">Descargar CV</a>
                 </div>
             </div>
         </section>
